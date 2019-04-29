98238ddc3454c0803788222eab8d1b0c2975f39b
Jamie Pham <jtkp66@gmail.com>
2019-04-29 14:27:55 -0700

    Added Survey Components and routes for said components.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import SurveyCreate from "./surveys/SurveyCreate";
+import SurveyList from "./surveys/SurveyList";
+import SurveyEdit from "./surveys/SurveyEdit";
+import SurveyShow from "./surveys/SurveyShow";
+import SurveyDelete from "./surveys/SurveyDelete";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={SurveyList} />
+          <Route path="/surveys/new" exact component={SurveyCreate} />
+          <Route path="/surveys/edit/:id" exact component={SurveyEdit} />
+          <Route path="/surveys/delete/:id" exact component={SurveyDelete} />
+          <Route path="/surveys/:id" exact component={SurveyShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
